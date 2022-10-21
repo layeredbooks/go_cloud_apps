8070b2b3096df6c015112e0733b2ff949c494770
layeredbooks <76998346+layeredbooks@users.noreply.github.com>
2022-10-21 08:19:31 +0200

    Initialize newsletter-confirm

---
+++ server/routes.go
@@ -0,0 +1,15 @@
+package server
+
+import (
+	"canvas/handlers"
+)
+
+func (s *Server) setupRoutes() {
+	handlers.Health(s.mux, s.database)
+
+	handlers.FrontPage(s.mux)
+	handlers.NewsletterSignup(s.mux, s.database, s.queue)
+	handlers.NewsletterThanks(s.mux)
+	handlers.NewsletterConfirm(s.mux, s.database, s.queue)
+	handlers.NewsletterConfirmed(s.mux)
+}
